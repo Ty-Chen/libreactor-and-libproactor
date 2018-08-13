f46124ebdded2b2aedf9cf829e74e7c3904a4e3d
ty <369494003@qq.com>
2018-08-13 23:19:29 +0800

    Modify event handler

--- reactor/event.h
+++ reactor/event.h
@@ -1,6 +1,7 @@
 #ifndef _EVENT_H
 #define _EVENT_H
 
+#if 0
 class Event_Handler {
 	// Abstract base class that serves as the
 	// target of the Initiation_Dispatcher.
@@ -12,6 +13,22 @@ class Event_Handler {
 	// I/O Handle.
 	virtual Handle get_handle (void) const = 0;
 };
+#endif
+
+class Event_Handler {
+public:
+	// Hook methods that are called back by the Initiation
+	// Dispatcher to handle particular types of events.
+	virtual int handle_accept      	  (void) = 0;
+	virtual int handle_input           (void) = 0;
+	virtual int handle_output          (void) = 0;
+	virtual int handle_timeout         (void) = 0;
+	virtual int handle_close (void) = 0;
+	// Hook method that returns the underlying
+	// I/O Handle.
+	virtual Handle get_handle (void) const = 0;
+};
+
 
 
 #endif
